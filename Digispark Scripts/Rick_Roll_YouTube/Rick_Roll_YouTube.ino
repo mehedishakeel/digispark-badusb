ad7c3996bef5176839566ac61871f86838d9f59f
Mehedi Shakeel <mehedishakeelpro@gmail.com>
2023-07-28 10:53:23 +0600

    DigiSpark Script
    BadUSB Scripts For Digispark

---
+++ Digispark Scripts/Rick_Roll_YouTube/Rick_Roll_YouTube.ino
@@ -0,0 +1,28 @@
+#include "DigiKeyboard.h"
+
+int led = 0;  // Change to pin 1 for MODEL A
+int on_wait = 1000;  // How long to toggle CAPS LOCK for.
+int off_wait = 5000;  // Delay between toggles.
+
+void setup() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(300);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.print("https://youtu.be/dQw4w9WgXcQ?t=43");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.sendKeyStroke(KEY_F11);
+  
+  pinMode(led, OUTPUT);
+  DigiKeyboard.update();
+}
+
+void loop() {
+  DigiKeyboard.sendKeyStroke(57);
+  digitalWrite(led, 1);
+  delay(on_wait);
+  DigiKeyboard.sendKeyStroke(57);
+  digitalWrite(led, 0);
+  delay(off_wait);
+}
